de45aacb949d584609fafa706f03db6db2fda367
Mathias Brulatout <mathias.brulatout@gmail.com>
2015-12-10 17:06:21 +0100

    missing utils + newtemplate for sw simulations

---
+++ src/rescue/model/rescue-utils.cc
@@ -0,0 +1,12 @@
+
+#include "rescue-utils.h"
+
+namespace ns3 {
+
+
+    bool CC_ENABLED = true;
+    bool CC_LOG_ENABLED = false;
+    bool ACK_ENABLED = false;
+
+    bool DISTRIBUTED_ACK_ENABLED = false;
+}
---
+++ src/rescue/model/rescue-utils.h
@@ -0,0 +1,20 @@
+/*
+ * File:   rescue-utils.h
+ * Author: tai
+ *
+ * Created on 17 novembre 2015, 11:14
+ */
+
+#ifndef RESCUE_UTILS_H
+#define	RESCUE_UTILS_H
+
+namespace ns3 {
+
+    extern bool CC_ENABLED;
+    extern bool CC_LOG_ENABLED;
+    extern bool ACK_ENABLED;
+
+    extern bool DISTRIBUTED_ACK_ENABLED;
+}
+#endif	/* RESCUE_UTILS_H */
+
